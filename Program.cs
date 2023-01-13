0c1290cc83c4c79674fd395d7f6aa1cff3ab2f7b
RanCwb <henriquesantos629@gmail.com>
2023-01-12 21:44:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+double precoOriginal;
+double porcentagemDesconto;
+double valorDesconto;
+double precoComDesconto;
+
+Console.WriteLine("Insira o preço original do produto:");
+precoOriginal = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira a porcentagem de desconto:");
+porcentagemDesconto = double.Parse(Console.ReadLine()) / 100;
+
+valorDesconto = precoOriginal * porcentagemDesconto;
+precoComDesconto = precoOriginal - valorDesconto;
+
+Console.WriteLine("Preço original: " + precoOriginal);
+Console.WriteLine("Desconto: " + porcentagemDesconto * 100 + "%");
+Console.WriteLine("Valor do desconto: " + valorDesconto);
+Console.WriteLine("Preço com desconto: " + precoComDesconto);
